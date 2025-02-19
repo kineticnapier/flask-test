9b2b2c633786877b35b08757e4815d9a6aca8b9e
kineticnapier <kiner5674@gmail.com>
2025-02-19 23:04:02 +0900

    Add query test page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,6 +9,15 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route("/redirect")
+def redirectpage():
+    return render_template('redirect.html')
+
+@app.route("/query")
+def querytest():
+    query = request.args.get('query')
+    return render_template('querytest.html', query=query)
 
 if __name__ == '__main__':
     app.run(debug=True)
